f11793187a6b5dad1cb2d17c5feac2532cfa46ad
Sunny kumar <120244832+sunny8750@users.noreply.github.com>
2023-07-05 13:33:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+function updateTimer() {
+      var currentTime = new Date();
+      var hours = currentTime.getHours();
+      var minutes = currentTime.getMinutes();
+      var seconds = currentTime.getSeconds();
+
+      // Add leading zeros to the hours, minutes, and seconds
+      hours = (hours < 10 ? "0" : "") + hours;
+      minutes = (minutes < 10 ? "0" : "") + minutes;
+      seconds = (seconds < 10 ? "0" : "") + seconds;
+
+      // Update the timer display
+      var timerElement = document.getElementById("timer");
+      timerElement.textContent = hours + ":" + minutes + ":" + seconds + ",";
+
+	 var currentDate = currentTime.toDateString();
+
+      // Update the date display
+      var dateElement = document.getElementById("date");
+      dateElement.textContent = currentDate;
+    }
+
+    // Update the timer every second
+    setInterval(updateTimer, 1000);
